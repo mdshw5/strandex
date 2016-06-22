2592631d40c8b653eafe8b1ee343b68896faa5b8
Matt Shirley <mdshw5@gmail.com>
2016-06-22 10:39:17 -0400

    Work with paired FASTQ files. Closes #2

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+install_requires = ['six']
+
+def get_version(string):
+    """ Parse the version number variable __version__ from a script. """
+    import re
+    version_re = r"^__version__ = ['\"]([^'\"]*)['\"]"
+    version_str = re.search(version_re, string, re.M).group(1)
+    return version_str
+
+setup(
+    name='strandex',
+    version=get_version(open('strandex/__init__.py').read()),
+    author='Matthew Shirley',
+    author_email='mdshw5@gmail.com',
+    url='https://github.com/mdshw5/strandex',
+    description='Strand-anchored regex for expansion or contraction of FASTQ files',
+    long_description=open('README.md').read(),
+    packages=['strandex'],
+    install_requires=install_requires,
+    entry_points = { 'console_scripts': [ 'strandex = strandex:main' ] },
+    license='MIT',
+    classifiers=[
+                "Development Status :: 3 - Alpha",
+                "License :: OSI Approved :: MIT License",
+                "Environment :: Console",
+                "Intended Audience :: Science/Research",
+                "Natural Language :: English",
+                "Operating System :: Unix",
+                "Programming Language :: Python :: 2.7",
+                "Programming Language :: Python :: 3.4",
+                "Topic :: Scientific/Engineering :: Bio-Informatics",
+                ],
+)
--- strandex/__init__.py
+++ strandex/__init__.py
@@ -3,16 +3,22 @@ import re
 import sys
 import argparse
 import random
+import fastq
 from six.moves import range
 
-pattern = re.compile(r'(@.+)[\n\r]+.+[\n\r]+\+.*?[\n\r].+[\n\r]')
+pattern = re.compile(r'@.+[\n\r]+.+[\n\r]+\+.*?[\n\r].+[\n\r]')
+__version__ = "0.1"
 
 def run(args):
-
-    file_size = os.path.getsize(args.fastq.name)
+    # Determine read size in bytes
+    with fastq.Reader(args.fastq1.name) as fq1:
+        read_size = len(str(next(fq1)))
+    # Determine file size in bytes
+    file_size = os.path.getsize(args.fastq1.name)
     random.seed(args.seed)
     n = 0
-    ahead = 4096
+    slop = 0
+    ahead = read_size * 2
     step = file_size // args.nreads
     if step < 1:
         step = 1
@@ -21,25 +27,31 @@ def run(args):
         i = next(idx)
         match = None
         while not match:
-            args.fastq.seek(i)
+            args.fastq1.seek(i)
             if i + ahead > file_size:
                 i = random.randrange(file_size)
             else:
-                chunk = args.fastq.read(ahead)
+                chunk = args.fastq1.read(ahead)
             match = re.search(pattern, chunk)
             if match:
                 a, b = match.span()
-                ahead = int((ahead + (b - a) * 4) / 2)
-                sys.stdout.write(match.group(0))
+                ahead = (b - a) * 2
+                args.out.write(match.group(0))
+                if args.fastq2:
+                    args.fastq2.seek(i + a)
+                    args.out2.write(args.fastq2.read(b-a))
                 n += 1
             else:
                 ahead += ahead
-    args.fastq.close()
+
 
 
 def main():
     parser = argparse.ArgumentParser(prog='strandex', description="sample uniformly without reading an entire fastq file")
-    parser.add_argument('fastq', type=argparse.FileType('r'), help="input fastq file")
+    parser.add_argument('fastq1', type=argparse.FileType('r'), help="input fastq file")
+    parser.add_argument('out', type=argparse.FileType('w'), help="output fastq file")
+    parser.add_argument('-fq2', '--fastq2', type=argparse.FileType('r'), help="input fastq file read pairs")
+    parser.add_argument('-o2', '--out2', type=argparse.FileType('w'), help="output fastq file read pairs")
     parser.add_argument('-n', '--nreads', type=int, default=1, help='number of reads to sample from input (default: %(default)s)')
     parser.add_argument('-s', '--seed', type=float, default=random.random(), help='seed for random number generator (default: None)')
     args = parser.parse_args()
