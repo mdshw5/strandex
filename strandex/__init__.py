f2a0fcf39808301e5890b0db6b3b73c64ef06800
Matt Shirley <mdshw5@gmail.com>
2016-06-23 10:25:39 -0400

    Bump version

--- strandex/__init__.py
+++ strandex/__init__.py
@@ -5,7 +5,7 @@ import argparse
 import random
 from six.moves import range
 
-__version__ = "0.3"
+__version__ = "0.3.1"
 
 class FastqSampler:
     pattern = re.compile(r'@.+[\n\r]+.+[\n\r]+\+.*?[\n\r].+[\n\r]')
